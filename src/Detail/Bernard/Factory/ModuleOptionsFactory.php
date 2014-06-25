fd171fb65c85722ab86b93f98cf3715a3211b707
Ivan Wolf <iwolf@detailag.ch>
2014-06-25 15:55:33 +0200

    Introduced config options for Bernard mail transport

--- src/Detail/Bernard/Factory/ModuleOptionsFactory.php
+++ src/Detail/Bernard/Factory/ModuleOptionsFactory.php
@@ -7,6 +7,8 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 
 use Detail\Bernard\Options\ModuleOptions;
 
+use RuntimeException;
+
 class ModuleOptionsFactory implements FactoryInterface
 {
     /**
@@ -15,6 +17,12 @@ class ModuleOptionsFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        return new ModuleOptions($serviceLocator->get('Config')['bernard']); /** @todo Check for existence */
+        $config = $serviceLocator->get('Config');
+
+        if (!isset($config['bernard'])) {
+            throw new RuntimeException('Config for Bernard is not set');
+        }
+
+        return new ModuleOptions($config['bernard']);
     }
 }
