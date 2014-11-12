e41e872e22e18949367e071fe2e9d2dd6b6adbb0
Ivan Wolf <eiwaen@gmail.com>
2014-11-12 15:34:38 +0100

    Moved Module.php to src directory

---
+++ src/Detail/Bernard/Module.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Detail\Bernard;
+
+use Zend\Loader\AutoloaderFactory;
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
+use Zend\ModuleManager\Feature\ControllerProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\Console\Adapter\AdapterInterface as Console;
+use Zend\Console\ColorInterface as Color;
+
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface,
+    ConsoleUsageProviderInterface,
+    ControllerProviderInterface,
+    ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            AutoloaderFactory::STANDARD_AUTOLOADER => array(
+                StandardAutoloader::LOAD_NS => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../../config/module.config.php';
+    }
+
+    public function getConsoleUsage(Console $console)
+    {
+        return array(
+            'Actions:',
+            'bernard consume [--max_runtime=] [--verbose|-v] <queue>' => 'Consume queue',
+            array('<queue>',          'The name of the queue to consume'),
+            array('--max_runtime', '(optional) Maximum runtime in seconds'),
+            array('--verbose|-v',     '(optional) Turn on verbose mode'),
+        );
+    }
+
+    public function getControllerConfig()
+    {
+        return array();
+    }
+
+    public function getServiceConfig()
+    {
+        return array();
+    }
+}
